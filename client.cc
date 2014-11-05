a8e0a036a574b990e1e2ccb5929bea15f249a929
Fengming Sun <s@sfmblog.cn>
2014-11-05 17:54:44 +0800

    for client

--- client.cc
+++ client.cc
@@ -7,14 +7,20 @@ int main(int argc, const char* argv[])
     try {
         ClientSocket client("127.0.0.1", 30000);
         std::string reply;
-        try {
-            client << "Test Message.";
-            client >> reply;
-        } catch (SocketException&) {}
-
-        std::cout << "We received this response from the server:" << std::endl;
-        std::cout << "\"" << reply << "\"" << std::endl;
-
+        std::string sendBuf;
+        while (true) {
+            std::cout << "Enter string send to server:" << std::endl;
+            std::cin >> sendBuf;
+            if (sendBuf.compare("quit") == 0) {
+                break;
+            }
+            try {
+                client << sendBuf;
+                client >> reply;
+            } catch (SocketException&) {}
+            std::cout << "We received this response from the server:" << std::endl;
+            std::cout << "\"" << reply << "\"" << std::endl;
+        }
     } catch (SocketException& e) {
         std::cout << "Exception was caught:" << e.description() << std::endl;
     }
